7bccb66e3df3c7e5c51f86676c850666e449fb48
thomp <thompsondjodjo021@gmail.com>
2025-02-24 13:19:37 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const dino = document.querySelector('.dino')
+    const grid = document.querySelector('.grid')
+    let gravity = 0.9
+    let isJumping = false
+    let isGamerOver = false
+    const alert = document.getElementById('alert')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
