1b938bac5ec791b1346b285da559871fcab2783e
thomp <thompsondjodjo021@gmail.com>
2025-02-24 13:19:37 +0100

    test

--- js/main.js
+++ js/main.js
@@ -51,26 +51,34 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 
+    function generationObstacles(){
+        if (!isGamerOver){
+            let randomTime = Math.random() * 4000
+            let obstaclePosition = 1000
+            const obstacle=  document.createElement('div')
+            obstacle.classList.add('obstacle')
+            grid.appendChild(obstacle)
+            obstacle.style.left = obstaclePosition + 'px'
 
+            let timerId = setInterval(function () {
+                if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60 ){
+                    clearInterval(timerId)
+                    alert.innerHTML  = 'Game Over'
+                    isGamerOver = true
 
+                    while (grid.firstChild) {
+                        grid.removeChild(grid.lastChild)
+                    }
 
+                }
 
+                obstaclePosition -=10
+                obstacle.style.left = obstaclePosition + 'px'
+            }, 20)
+            setTimeout(generationObstacles, randomTime)
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 
 
 
