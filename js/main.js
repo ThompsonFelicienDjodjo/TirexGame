569f7b36ab077da13e033b9eed41b5d342bc65eb
thomp <thompsondjodjo021@gmail.com>
2025-02-24 13:19:37 +0100

    Initial commit

--- js/main.js
+++ js/main.js
@@ -9,6 +9,58 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+    function control(e) {
+        if (e.code=== "Space"){
+            if (!isJumping){
+                jump()
+                console.log('jump')
+            }
+        }
+    }
+    document.addEventListener('keydown', control)
+
+    let position = 0
+
+    function jump (){
+        isJumping = true
+        let count = 0
+        let timerId = setInterval(function () {
+
+            position += 30
+            count ++
+            position = position * gravity
+            dino.style.bottom = position + 'px'
+
+
+            if (count === 15){
+                clearInterval(timerId)
+                let downTimerId = setInterval(function () {
+                    if (count === 0) {
+                        clearInterval(downTimerId)
+                        isJumping = false
+                    }
+                    position -=5
+                    count --
+                    position = position * gravity
+                    dino.style.bottom = position + 'px'
+                }, 20)
+            }
+
+
+        }, 20)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
